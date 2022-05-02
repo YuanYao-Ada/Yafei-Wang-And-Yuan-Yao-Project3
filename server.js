9542c5ce2e7d41aac3d73a8a0d11a04837438f77
Yuan Yao <adayuanyao@gmail.com>
2022-05-02 16:02:14 -0700

    Update configuration

--- server.js
+++ server.js
@@ -27,12 +27,22 @@ const auth_middleware = require('./routes/middleware/auth_middleware');
 
 console.log("Connect to DB Successfully");
 
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/articles', articleRouter);
 app.use('/api/user', userRouter);
+
+const cors = require('cors');
+app.use(cors({
+  origin: '*',
+}));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 8000, function() {
     console.log("Starting server");
